86e8c22d6992e27dc21e0e4e3e559f5bf94d1a67
Dominic Scotto <dominicscotto@gmail.com>
2013-11-20 15:50:35 -0800

    Miles Told me to do this so I am going to do it this way.
    VERBOSE! This is a rails shell FYI

---
+++ RevengeCicle/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RevengeCicle::Application.config.secret_token = 'bd341e50ae9e3eebe30cede8d8fe052635dd3b8cf2cacdcde02db13c8ab6013f542c8746d3e0a47c53d18af648076ac7641d632092fc3a5c76787ef6166ccb3d'
