f22f0152bc76aa8bff3c0317de220ef6afee1214
Apprentice <apprentice@dbc-29.local>
2013-11-21 21:50:33 -0800

    Comment ajax fixed.

--- RevengeCicle/app/assets/javascripts/post.js
+++ RevengeCicle/app/assets/javascripts/post.js
@@ -8,7 +8,18 @@ $(function(){
 		debugger
 	}
 
+  function onSuccess(e, response){
+    console.log(response)
+    $("ul").append(response);
+  }
+
+  function onError(e, xhr) {
+    debugger
+  }
 	$(".posting form").on('ajax:success', onSuccess)
 	$(".posting form").on('ajax:error', onError)
+  $(".commenting form").on('ajax:success', onSuccess)
+  $(".commenting form").on('ajax:error', onError)
+
 
 })
