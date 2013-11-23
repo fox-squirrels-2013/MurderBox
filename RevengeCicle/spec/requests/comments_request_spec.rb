1dd7348aefa97ed5808a52717f7e6f58a470c1c0
Apprentice <apprentice@dbc-29.local>
2013-11-22 16:11:59 -0800

    Comments spec.

--- RevengeCicle/spec/requests/comments_request_spec.rb
+++ RevengeCicle/spec/requests/comments_request_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 describe "Comments Request" do
 	it "creates an upvote when the upvote button is clicked" do
-		expect{
-		post = Post.create(title: "fake", body: "fake", author: "fake", revengemail:"fake")
-		comment = Comment.create(:author => 'Miya', :body => 'this is the body', :post_id => post.id)
-		put "/comments/#{comment.id}", {comment_id: comment.id, vote_type: 'up', post_id: post.id} 
-		}.to change{comment.vote.count}.by(1)
+    post = Post.create(title: "fake", body: "fake", author: "fake", revengemail:"fake")
+    comment = Comment.create(:author => 'Miya', :body => 'this is the body', :post_id => post.id)
+    expect{
+		put comment_path(comment), {comment_id: comment.id, vote_type: 'up', post_id: post.id}
+    comment.reload
+		}.to change{comment.votes}.by(1)
 	end
 
-	it "lowers the upvote when the downvote botton is clicked"
+	it "lowers the upvote when the downvote button is clicked" do
+    post = Post.create(title: "fake", body: "fake", author: "fake", revengemail:"fake")
+    comment = Comment.create(:author => 'Miya', :body => 'this is the body', :post_id => post.id)
+    expect{
+    put comment_path(comment), {comment_id: comment.id, vote_type: 'down', post_id: post.id}
+    comment.reload
+    }.to change{comment.votes}.by(-1)
+  end
 
-
-
-end	
+end
